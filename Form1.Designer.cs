e91d41414710b4bb09e85a6027c75eafa3089dd6
Titanic880 <Warframeia1501@gmail.com>
2021-04-18 11:07:52 -0600

    implemented a couple features from my assignment 6, Almost finished the Send File button, just needs a method to pass the object to so it can be sent

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,98 @@ namespace FinalProj_Helper
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PbScreenShare = new System.Windows.Forms.PictureBox();
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.LstChat = new System.Windows.Forms.ListBox();
+            this.LblChat = new System.Windows.Forms.Label();
+            this.TbChatMessage = new System.Windows.Forms.TextBox();
+            this.BtnSendFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PbScreenShare)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PbScreenShare
+            // 
+            this.PbScreenShare.Dock = System.Windows.Forms.DockStyle.Right;
+            this.PbScreenShare.Location = new System.Drawing.Point(297, 0);
+            this.PbScreenShare.Name = "PbScreenShare";
+            this.PbScreenShare.Size = new System.Drawing.Size(503, 450);
+            this.PbScreenShare.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PbScreenShare.TabIndex = 0;
+            this.PbScreenShare.TabStop = false;
+            this.PbScreenShare.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PbScreenShare_MouseClick);
+            this.PbScreenShare.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.PbScreenShare_PreviewKeyDown);
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Location = new System.Drawing.Point(5, 360);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(140, 23);
+            this.BtnSend.TabIndex = 1;
+            this.BtnSend.Text = "Send Message";
+            this.BtnSend.UseVisualStyleBackColor = true;
+            // 
+            // LstChat
+            // 
+            this.LstChat.FormattingEnabled = true;
+            this.LstChat.Location = new System.Drawing.Point(5, 233);
+            this.LstChat.Name = "LstChat";
+            this.LstChat.Size = new System.Drawing.Size(286, 95);
+            this.LstChat.TabIndex = 2;
+            // 
+            // LblChat
+            // 
+            this.LblChat.AutoSize = true;
+            this.LblChat.Location = new System.Drawing.Point(2, 217);
+            this.LblChat.Name = "LblChat";
+            this.LblChat.Size = new System.Drawing.Size(50, 13);
+            this.LblChat.TabIndex = 3;
+            this.LblChat.Text = "Chat Box";
+            // 
+            // TbChatMessage
+            // 
+            this.TbChatMessage.Location = new System.Drawing.Point(5, 334);
+            this.TbChatMessage.Name = "TbChatMessage";
+            this.TbChatMessage.Size = new System.Drawing.Size(286, 20);
+            this.TbChatMessage.TabIndex = 4;
+            this.TbChatMessage.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.TbChatMessage_PreviewKeyDown);
+            // 
+            // BtnSendFile
+            // 
+            this.BtnSendFile.Location = new System.Drawing.Point(151, 360);
+            this.BtnSendFile.Name = "BtnSendFile";
+            this.BtnSendFile.Size = new System.Drawing.Size(140, 23);
+            this.BtnSendFile.TabIndex = 5;
+            this.BtnSendFile.Text = "Send File";
+            this.BtnSendFile.UseVisualStyleBackColor = true;
+            this.BtnSendFile.Click += new System.EventHandler(this.BtnSendFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnSendFile);
+            this.Controls.Add(this.TbChatMessage);
+            this.Controls.Add(this.LblChat);
+            this.Controls.Add(this.LstChat);
+            this.Controls.Add(this.BtnSend);
+            this.Controls.Add(this.PbScreenShare);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PbScreenShare)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox PbScreenShare;
+        private System.Windows.Forms.Button BtnSend;
+        private System.Windows.Forms.ListBox LstChat;
+        private System.Windows.Forms.Label LblChat;
+        private System.Windows.Forms.TextBox TbChatMessage;
+        private System.Windows.Forms.Button BtnSendFile;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Standards.Network;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,108 @@ namespace FinalProj_Helper
 {
     public partial class Form1 : Form
     {
+        //Recieves messages/files
+        readonly Queue<object> IncomingData = new Queue<object>();
+        Helper_Connection connection;
+
+
+        //Message history system
+        private readonly List<string> messages = new List<string>();
+        private int messagesPOS = 0;
+
         public Form1()
         {
             InitializeComponent();
+            KeyPreview = true;
+
+            messages.Add("");
+        }
+
+
+        private void PbScreenShare_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void PbScreenShare_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+
+        private void TbChatMessage_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (messagesPOS < 0) //Loops to top
+                messagesPOS = messages.Count;
+            else if (messagesPOS > messages.Count) //Loops to bottom
+                messagesPOS = -1;
+
+            //Switch of all used keys
+            switch (e.KeyCode)
+            {
+                //Cycles to newer message
+                case Keys.Down:
+                    messagesPOS++;
+
+                    if (messagesPOS < 0) //Loops to top
+                        messagesPOS = messages.Count - 1;
+                    else if (messagesPOS >= messages.Count) //Loops to bottom
+                        messagesPOS = 0;
+
+                    if (messages[messagesPOS] == null)
+                        return;
+                    else
+                        TbChatMessage.Text = messages[messagesPOS];
+                    break;
+                //Cycles to older message
+                case Keys.Up:
+                    messagesPOS--;
+
+                    if (messagesPOS < 0) //Loops to top
+                        messagesPOS = messages.Count - 1;
+                    else if (messagesPOS > messages.Count) //Loops to bottom
+                        messagesPOS = 0;
+
+                    if (messages[messagesPOS] == null)
+                        return;
+                    else
+                        TbChatMessage.Text = messages[messagesPOS];
+                    break;
+
+                //Sends a message if enter is pressed
+                case Keys.Enter:
+                    SendToHostee();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void BtnSendFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Choose a file to send";
+            ofd.Multiselect = false;
+            DialogResult res =  ofd.ShowDialog();
+            if(res == DialogResult.OK)
+            {
+                string tmp = ofd.FileName.Split('\\')[ofd.FileName.Split('\\').Length - 1];
+                File_Standard fs = new File_Standard
+                {
+                    //Grabs the file Name
+                    File_Name = tmp.Split('.')[tmp.Split('.').Length - 1],
+                    //Grabs the last object that occurs after a period (File Extension)
+                    File_Ext = ofd.FileName.Split('.')[ofd.FileName.Split('.').Length - 1],
+                    //Gets the contents and sets the file flag
+                    File_Contents = File.ReadAllBytes(ofd.FileName),
+                    Is_File = true
+                };
+
+                SendToHostee(fs);
+            }
+            else
+                MessageBox.Show("File selection cancelled");
+            
         }
     }
 }
